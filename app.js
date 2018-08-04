bf563b7d1bcd7e925bae81f3cabab6e3b03ac890
mtotodev05 <mtotodev05@gmail.com>
2018-08-04 07:09:40 +0300

    Error Handling

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+
 const productRoutes = require('./api/routes/product');
 const orderRoutes = require('./api/routes/order');
 
+//Order Routes
+app.use(morgan('dev'));
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
 
+
+/*Error Handlers*/
+app.use(function(req,res,next){
+   const error = new Error('Not Found');
+   error.status =404;
+   next(error);
+});
+app.use(function(err,req,res,next){
+   res.status(err.status || 500);
+   res.json({
+      err:{
+          message:err.message
+      }
+   });
+});
 module.exports = app;
