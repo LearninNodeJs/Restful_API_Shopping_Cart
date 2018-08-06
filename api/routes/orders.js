85195b1f9e5ffed6c88e068620bf1fdd74fa9851
mtotodev05 <mtotodev05@gmail.com>
2018-08-06 15:31:30 +0300

    Populating end points with reference data about objects

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -6,7 +6,9 @@ const Product = require('../models/product');
 
 
 router.get('/',function(req,res,next){
-   Order.find().select('_id product quantity').exec()
+   Order.find().select('_id product quantity')
+       .populate('product','name')
+       .exec()
        .then(result =>{
             console.log(result);
             res.status(201).json({
@@ -67,25 +69,50 @@ router.post('/',function(req,res,next){
        });
 
 });
+router.get('/:productId',function(req,res,next){
+   const productId = req.params.productId;
+   Order.findById(productId).select('product _id quantity')
+       .populate('product','name')
+       .exec()
+       .then(order => {
+          if(!order){
+             return res.status(404).json({
+                message: 'No Record Has been Found'
+             });
+          }
+            res.status(200).json({
+                message:'Order Has Been Found',
+                order:order,
+                request:{
+                   type:'GET',
+                   url:'http://localhost:3000/api/v1/orders/'+order._id
+                }
+
+            });
+       })
+       .catch(err=>{
+          res.status(500).json({
+             message:'Record Not Found',
+             error:err.message
+          });
+       });
+});
+router.delete('/:orderId',function(req,res,next){
+   const orderId = req.params.orderId;
+   Order.remove({_id:orderId})
+       .exec()
+       .then(result =>{
+
+          res.status(200).json({
+             message:'Successfully Deleted Record',
+             result
+          });
+       })
+       .catch(error => {
+          res.status(500).json({
+             message:'Error Deleting Record',
+             error:error.message
+          });
+       });
+});
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
